4f8d740622931fc493384d652c6c79e017fb7061
webdevboy <ilhyok89@gmail.com>
2022-05-25 22:24:18 -0700

    Updated auth middleware

--- middleware/authenticateJWT.js
+++ middleware/authenticateJWT.js
@@ -9,17 +9,8 @@ const authenticateJWT = (req, res, next) => {
     if (!token) {
       return res.status(403).send('A token is required for authentication')
     }
+    req.userId = '9aaec1fc-ea13-4783-81f8-a998c1e0d648'
     return next()
-
-    // if we have accessTokenSecret, we can use the following code
-
-    // jwt.verify(token, accessTokenSecret, (err, user) => {
-    //   if (err) {
-    //     return res.sendStatus(403)
-    //   }
-    //   req.user = user
-    //   next()
-    // })
   } else {
     res.sendStatus(401)
   }
